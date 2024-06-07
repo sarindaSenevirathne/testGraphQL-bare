b91458659ed3f46a74b27f2dc0c5701def978a0b
David Halmy <David.Halmy@us.kbr.com>
2024-06-07 12:31:14 -0400

    just run just run?

--- justrun.py
+++ justrun.py
@@ -1,10 +1,1 @@
-from flask import Flask, jsonify, request
-import graphene
-from graphene import ObjectType, String, Int, List, Field
-
-app = Flask(__name__)
-
-
-
-if __name__ == '__main__':
-    app.run()
+print("Hello")
