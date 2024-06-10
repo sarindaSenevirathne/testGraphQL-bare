8e00eeb728738834f8aef5b69c0b74f301764a24
David Halmy <David.Halmy@us.kbr.com>
2024-06-10 11:37:07 -0400

    changed to work

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ import graphene
 
 app = Flask(__name__)
 
-# Sample data
 books = [
     {
-        "author": "me",
-        "id": 1,
-        "name": "adam sandler",
+        "id": "1",
+        "name": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
         "status": "read"
+    },
+    {
+        "id": "2",
+        "name": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "status": "unread"
+    },
+    {
+        "id": "3",
+        "name": "1984",
+        "author": "George Orwell",
+        "status": "in progress"
     }
 ]
 
 class Book(graphene.ObjectType):
-    id = graphene.String(description="ID of the book", required=True)
+    id = graphene.String(description = " number of the book", required = True)
     name = graphene.String(description="Name of the book", required=True)
     author = graphene.String(description="Author of the book", required=True)
     status = graphene.String(description="Status of the book", required=True)
@@ -24,15 +35,35 @@ class Query(graphene.ObjectType):
 
     def resolve_all_books(self, info):
         return books
+    
+class Query(graphene.ObjectType):
+    all_books = graphene.List(Book)
 
-schema = graphene.Schema(query=Query)
+    def resolve_all_books(self, info):
+        return books
+
+
+class Mutation(graphene.ObjectType):
+    add_book = graphene.Field(Book,
+                              name = graphene.String(required=True),
+                              author = graphene.String(required=True),
+                              status = graphene.String(required=True)
+                              )
+
+    def resolve_add_book(self, info, name, author, status):
+        new_id = len(books) + 1
+        new_book = Book(id = new_id, name = name, author = author, status = status)
+        books.append(new_book)
+        return new_book
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
 
 @app.route('/graphql', methods=['POST'])
 def graphql_endpoint():
     data = request.get_json()
     result = schema.execute(data.get('query'))
-    if result.errors:
-        return jsonify({'errors': [str(e) for e in result.errors]})
     return jsonify(result.data)
 
 if __name__ == '__main__':
